aad47d52fee574eb1c9f63e08bcd76cf8fa936f6
Jose Delgado Matamoros <alonsodelm@gmail.com>
2022-04-22 22:46:01 -0600

    get from body

--- src/controllers/cache/update.ts
+++ src/controllers/cache/update.ts
@@ -3,7 +3,8 @@ import requestMiddleware from '../../middleware/request-middleware';
 import Cache from '../../models/Cache';
 
 const update: RequestHandler = async (req: Request, res) => {
-  const { key, value } = req.params;
+  const { key } = req.params;
+  const { value } = req.body;
 
   const cache = await Cache.findOneAndUpdate({key}, {value});
   if (!cache) {
